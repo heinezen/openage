92a0e81f063cd9847d7a1fb5270e3b855624ff38
Christoph Heine <heinezen@hotmail.de>
2017-10-22 03:38:19 +0200

    Move mockups to gh-pages.

--- libopenage/util/misc.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2013-2015 the openage authors. See copying.md for legal info.
-
-#include "misc.h"
-
-namespace openage {
-namespace util {
-
-} // namespace util
-} // namespace openage
--- openage/util/fslike/cpp.pyx
+++
@@ -1,5 +0,0 @@
-# Copyright 2015-2016 the openage authors. See copying.md for legal info.
-
-
-def setup():
-    pass
